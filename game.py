ef5cee27ca4f8bf187e6b875aaa0804580eaad57
kylee215 <108580296+kylee215@users.noreply.github.com>
2022-07-12 19:54:19 -0400

    Create game.py
    processing user inputs

---
+++ game.py
@@ -0,0 +1,28 @@
+print("Rock, Paper, Scissors, Shoot!")
+# this is the "game.py" file
+# Processing user inputs
+user_choice = input("Please make a selection ('rock', 'paper', 'scissors'):")
+print("You chose:", user_choice)
+
+
+# Validating user inputs
+
+
+# Simulating computer selection
+
+
+# Determining the winner
+
+
+# Displaying results
+
+#-------------------
+#Welcome 'Player One' to my Rock-Paper-Scissors game...
+#-------------------
+#Please choose either 'rock', 'paper', or 'scissors': rock
+#You chose: 'rock'
+#The computer chose: 'paper'
+#-------------------
+#Oh, the computer won. It's ok.
+#-------------------
+#Thanks for playing. Please play again!
